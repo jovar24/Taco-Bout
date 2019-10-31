a84cb649c93ead225455c47ece4c400e9366c56d
Jose Vargas <jovar2497@gmail.com>
2019-10-30 21:41:47 -0600

    edited about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React from "react";
+import "../style/about.scss"
 
 const About = () => {
   return (
     <div className="About">
-      <div className="wrapper">
-        <p>lorem*6</p>
+      <div className="background-about">
+        <div className="square">
+
+          <h1>Taco Bout</h1>
+
+          <img className="firstImage" src="https://source.unsplash.com/random" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi blanditiis laborum dicta quod necessitatibus ratione labore repellat. Saepe, molestiae ut. Dolores, nisi voluptatem laborum dolorum perferendis fugiat a ea adipisci.
+            Earum quisquam ab neque dolore eaque eius ratione ullam a repellat quasi officiis illo, exercitationem molestias temporibus dolor eligendi. Aperiam soluta sapiente veniam voluptas ullam sint modi reprehenderit ipsum placeat!
+            Quidem optio cumque dignissimos iure, voluptates aliquam deserunt aspernatur alias autem earum beatae? Veniam tempore cum amet recusandae quis iusto praesentium ullam asperiores architecto doloremque. Aspernatur doloremque culpa repellat ratione?
+            Non quaerat recusandae eum aperiam? </p>
+          <img className="secondImage" src="https://source.unsplash.com/random" style={{ float: "right", margin: "10px 0px 20px 40px" }} />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi blanditiis laborum dicta quod necessitatibus ratione labore repellat. Saepe, molestiae ut. Dolores, nisi voluptatem laborum dolorum perferendis fugiat a ea adipisci.
+            Earum quisquam ab neque dolore eaque eius ratione ullam a repellat quasi officiis illo, exercitationem molestias temporibus dolor eligendi. Aperiam soluta sapiente veniam voluptas ullam sint modi reprehenderit ipsum placeat!
+            Quidem optio cumque dignissimos iure, voluptates aliquam deserunt aspernatur alias autem earum beatae? Veniam tempore cum amet recusandae quis iusto praesentium ullam asperiores architecto doloremque. Aspernatur doloremque culpa repellat ratione?
+            Non quaerat recusandae eum aperiam? </p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi blanditiis laborum dicta quod necessitatibus ratione labore repellat. Saepe, molestiae ut. Dolores, nisi voluptatem laborum dolorum perferendis fugiat a ea adipisci.
+          Earum quisquam ab neque dolore eaque eius ratione ullam a repellat quasi officiis illo, exercitationem molestias temporibus dolor eligendi. Aperiam soluta sapiente veniam voluptas ullam sint modi reprehenderit ipsum placeat!
+          Quidem optio cumque dignissimos iure, voluptates aliquam deserunt aspernatur alias autem earum beatae? Veniam tempore cum amet recusandae quis iusto praesentium ullam asperiores architecto doloremque. Aspernatur doloremque culpa repellat ratione?
+            Non quaerat recusandae eum aperiam? </p>
+        </div>
+
       </div>
     </div>
   )
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../style/home.scss"
-import logo from "../logo.png"
+import logo from "../images/logo.png"
 
 const Home = () => {
   return (
