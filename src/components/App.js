c8aac848758366efb17ec642682c3d9564e32d62
Jose Vargas <jovar2407@gmail.com>
2019-11-03 19:48:57 -0700

    deleted login and uth page

--- src/components/App.js
+++ src/components/App.js
@@ -10,29 +10,15 @@ import Events from "./pages/events";
 import Contact from "./pages/contact"
 import NavConatiner from "./navigation/navigation-container";
 
-import "./style/App.scss";
+
+import "../style/App.scss";
+
 
 
 
 export default class App extends Component {
-  constructor() {
-    super()
-
-    this.getLoginInfo = this.getLoginInfo.bind(this)
-  }
-  getLoginInfo() {
-    axios
-      .get("http://localhost:5000/users")
-      .then(response => {
-        console.log("response data", response);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
 
   render() {
-    this.getLoginInfo();
     return (
       <div className="App">
         <div className="container">
@@ -44,6 +30,7 @@ export default class App extends Component {
               <Route path="/About" component={About} />
               <Route path="/Events" component={Events} />
               <Route path="/Contact" component={Contact} />
+
             </Switch>
           </BrowserRouter>
         </div>
